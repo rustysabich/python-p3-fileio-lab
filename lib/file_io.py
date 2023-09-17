eb03ab8e9ddbe48ddb783ae576e3421769258995
evansochieng <evansochieng121@gmail.com>
2023-09-17 21:16:53 +0300

    practice file i/o

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    # write the content to a .txt file with the names passed
+    with open(f'{file_name}.txt', mode='w') as my_file:
+        my_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a') as my_file:
+        my_file.write(append_content)
+        my_file.close()
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode='r') as my_file:
+        return my_file.read()
